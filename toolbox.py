0fc73f933ed196c263fa5db5c7fb7097bbc86aab
ch4r7 <charlieboulo@gmail.com>
2014-11-10 10:10:44 -0500

    Hex file editor improved with edition/insertion features and a less-like display function

--- toolbox.py
+++ toolbox.py
@@ -8,7 +8,7 @@ import rlcompleter
 import readline
 
 #	TODO :
-# hex editor
+# hex editor [Done]
 # multibase (de)encoder 
 # TCP/http network crafter
 # adb scripter
@@ -17,27 +17,104 @@ import readline
 class Hxf:
 	"""Hexadecimal file editor class
 	"""
+	
 	def __init__(self, filepath=None):
 		if filepath:
 			self.filepath = filepath
 			self.f = open(filepath,'rb')
-			self.f.seek(0, os.SEEK_END)
-			self.filelength = self.f.tell()
-			self.f.seek(0, os.SEEK_SET)
-			
+			self.rseek()
+			self.filelength = int(self.f.tell())
+			self.reset()
+	
+	def __len__(self):
+		return self.filelength
 		
 	def __del__(self):
 		if self.filepath and self.f:
-			self.f.close()
+			self.close()
 	
-	def rdisp(self, size=32, offset=0,  enable_ascii_repr = True):
-		"""Display size file's bytes from len(file) - offset in hexadecimal representation
+	def close(self):
+		self.f.close()
+		
+	def flush(self):
+		self.f.flush()
+		
+	def seek(self,offset=0):
+		self.f.seek(offset, os.SEEK_SET)
+		
+	def rseek(self,offset=0):
+		self.f.seek(offset, os.SEEK_END)
+	
+	def reset(self):
+		"""Reset internal file cursor to the begining
+		"""
+		if self.f:
+			self.seek()
+			
+	def read(self,size):
+		return self.f.read(size)
+	
+	def write(self,out):
+		return self.f.write(out)
+	
+	def read_list(size , off_start=0):
+		"""read size bytes from off_start and return a list containing
+		each chars int values
+		"""
+		self.seek(off_start)
+		return [ord(d) for d in self.read(size)]
+	
+	def get_list(self,off_start,off_stop):
+		"""Return byte list from start offset to stop offset
+		"""
+		self.reset()
+		self.seek(off_start)
+		return [ord(d) for d in self.read( off_stop - off_start )]
+		
+	def set(self,off_start,output):
+		"""Replace and potentially expand file with byte_array values
+		from file's off_start offset
+		"""
+		self.close()
+		self.f = open(self.filepath,'r+')
+		
+		self.seek(off_start)
+		if type(output) == str:
+			self.write(output)
+		elif type(output) == list:
+			if len(output) > 0  and type(output[0])== int:
+				self.write("".join([chr(c) for c in output]))
+		
+		self.flush()
+		self.close()
+		self.f = open(self.filepath,'rb')
+		
+	def rdisp(self, size=0, offset=0,  enable_ascii_repr = True):
+		"""Display size file's bytes from len(file) - offset in
+		hexadecimal representation
 		"""
 		return self.disp(size, self.filelength - offset,  enable_ascii_repr)
 		
+	def less(self):
+		self.disp(16)
+		offset  = 16
+		try:
+			while(True):
+				if offset > len(self):
+					break
+				self._disp(16,offset,True,False)
+				raw_input()
+				offset+=16
+		except KeyboardInterrupt:
+			pass
+	
 	def disp(self, size=0,  offset=0,enable_ascii_repr = True):
-		"""Display size file's bytes from offset in hexadecimal representation
+		"""Display size file's bytes from offset in hexadecimal
+		representation
 		"""
+		return self._disp(size,offset,enable_ascii_repr,True)
+		
+	def _disp(self, size=0,  offset=0,enable_ascii_repr = True, enable_top_border = True):
 		sep_char = " "
 		line_byte_length = 16
 	
@@ -48,8 +125,8 @@ class Hxf:
 		# size fix if too long
 		if size + offset > self.filelength:
 			size = self.filelength - offset
-		
-		ret = "%05X%s%s"%(0,sep_char,sep_char.join(["%02X"%d for d in range(line_byte_length)]))
+			
+		ret = "%05X%s%s"%(0,sep_char,sep_char.join(["%02X"%d for d in range(line_byte_length)])) if enable_top_border else ""
 		
 		if self.f:
 			self.f.seek(offset)
@@ -57,14 +134,14 @@ class Hxf:
 			
 			for i in range(size/line_byte_length):
 				values = self.f.read(line_byte_length)
-				ret += "%05X%s%s"%(i*line_byte_length,sep_char,sep_char.join(["%02X"%ord(d) for d in values]))
+				ret += "%05X%s%s"%(offset+i*line_byte_length,sep_char,sep_char.join(["%02X"%ord(d) for d in values]))
 				if enable_ascii_repr:
 					ret += " | %s |"%("".join([ "%c"%v if (ord(v)<126 and ord(v)>=32) else "." for v in values]))
 				ret += "\n"
 			
 			if size%line_byte_length > 0 :
 				values = self.f.read( size%line_byte_length )
-				ret += "%05X%s%s"%((size/line_byte_length)*line_byte_length,sep_char,sep_char.join(["%02X"%ord(d) for d in values ]))
+				ret += "%05X%s%s"%(offset+(size/line_byte_length)*line_byte_length,sep_char,sep_char.join(["%02X"%ord(d) for d in values ]))
 				#Space padding
 				ret += (line_byte_length - len(values))*"   "
 				
@@ -72,10 +149,6 @@ class Hxf:
 					ret += " | %s |"%("".join([ "%c"%v if (ord(v)<126 and ord(v)>=32) else "." for v in values]))
 			
 		print ret
-		
-	#~ def get(self):
-	
-	
 	
 class Cp:
 	"""Class used to compress bytestring
